33b5e6b87007e74d57750dd7beb0f426f41c2ba4
Levis <alevis@users.noreply.github.com>
2023-07-13 13:50:07 -0400

    Add logos

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import '../styles/globals.css';
 import { getGlobalData } from '../lib/cosmic';
 import Generator from 'next/font/local';
-import Banner from '../components/Banner';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -25,7 +24,6 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   return (
     <html lang='en' className={`${sans.variable} font-sans`}>
       <body className='bg-white dark:bg-zinc-950'>
-        <Banner />
         <Header name={siteData} />
         {children}
         <Footer />
